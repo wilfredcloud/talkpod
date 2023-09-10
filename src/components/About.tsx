367f7d52a117c385afc37b6d8961f2910234e0b9
wilfredcloud <wilfredcloudspace@gmail.com>
2023-09-10 13:28:06 +0100

    feat: Add about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,7 +5,7 @@ const About = () => {
     <h2>About</h2>
         <div> 
         One of my big dream is to build a voice and video conferencing application. This lead to the development of 
-        talkpod which is the future of voice conferencing, designed to empower individuals and organizations to connect, collaborate, and communicate seamlessly. With a commitment to delivering unparalleled audio quality and a suite of powerful features, ConnectVoice redefines what it means to hold productive meetings, webinars, and group discussions in today's fast-paced world.
+        talkpod which is the future of voice conferencing, designed to empower individuals and organizations to connect, collaborate, and communicate seamlessly. With a commitment to delivering unparalleled audio quality and a suite of powerful features, Talkpod redefines what it means to hold productive meetings, webinars, and group discussions in today's fast-paced world.
         </div>
 </div>
   )

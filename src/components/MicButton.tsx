e4fea6c8f878628ca5c6c72634d32c01bcc961c9
wilfredcloud <wilfredcloudspace@gmail.com>
2023-09-14 21:42:28 +0100

    Add micbutton

---
+++ src/components/MicButton.tsx
@@ -0,0 +1,19 @@
+import { Tooltip } from 'antd'
+import { useState } from 'react'
+import { BsMicFill, BsMicMuteFill } from 'react-icons/bs'
+
+const MicButton = () => {
+    const [isMuted, setIsMuted] = useState(true);
+
+    const toggleMute = () => {
+        setIsMuted((prev) => !prev)
+    }
+  return (
+    <Tooltip placement='top' title={isMuted? "Unmute your mic" : "Mute your mic"}>
+    <div onClick={toggleMute} className={`rounded-full h-12 w-12 
+  flex flex-row items-center justify-center text-xl cursor-pointer ${isMuted ? "bg-slate-800" : "bg-blue-700" }`}>{isMuted ? <BsMicMuteFill/> : <BsMicFill/>}</div>
+    </Tooltip>
+  )
+}
+
+export default MicButton

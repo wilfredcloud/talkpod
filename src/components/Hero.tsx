b0b0186619056ed9e5e38d493a60401e6bde07da
wilfredcloud <wilfredcloudspace@gmail.com>
2023-09-18 04:07:50 +0100

    Update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,31 +3,31 @@ import callSVG from '../assets/svg.svg'
 const Hero = () => {
   return (
     <div className="showcase">
-    <div className="showcase-content row">
-      <div className="left col-12 col-md-6">
-        <h1>Welcome to <em>Talkpod!</em></h1>
-        <p>
-          The Conferencing Solution to amplify your voice
+      <div className="showcase-content row">
+        <div className="left col-12 col-md-6">
+          <h1>Welcome to <em>Talkpod!</em></h1>
+          <p>
+            The Conferencing Solution to amplify your voice
 
-        </p>
-        <div className="btn-group">
-        <button
-          className="btn-full"
-        >
-          Get Started!
-        </button>
-        <button
-          className="btn-border ml-2"
-        >
-          Github
-        </button>
+          </p>
+          <div className="btn-group">
+            <button
+              className="btn-full"
+            >
+              Get Started!
+            </button>
+            <button
+              className="btn-border ml-2"
+            >
+              Github
+            </button>
+          </div>
+        </div>
+        <div className="right col-12 col-md-6">
+          <img src={callSVG} alt="call svg" />
         </div>
       </div>
-      <div className="right col-12 col-md-6">
-        <img src={callSVG} alt="call svg" />
-      </div>
     </div>
-  </div>
   )
 }
 

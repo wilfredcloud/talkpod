f538ead09bbd62c6910b5ce79cc09ca36abec2e5
wilfredcloud <wilfredcloudspace@gmail.com>
2023-09-18 03:04:14 +0100

    Add usercontext

---
+++ src/contexts/UserContext.tsx
@@ -0,0 +1,52 @@
+import {createContext, ReactNode, useEffect, useState } from "react";
+
+interface UserType {
+    data: {
+        id: string;
+        name: string;
+        email: string;
+    };
+    token: string
+}
+
+interface UserContextValueType {
+    user: UserType | null;
+    setUser: React.Dispatch<React.SetStateAction<UserType | null>>;
+}
+
+const storedUser = localStorage.getItem('pod_user');
+
+const initialData = storedUser ? JSON.parse(storedUser) : null;
+
+export const UserContext = createContext<UserContextValueType>({
+    user: initialData,
+    setUser: () => { },
+});
+
+interface UserProviderProps {
+    children: ReactNode
+}
+
+const UserProvider:React.FC<UserProviderProps> = ({ children }) => {
+    const [user, setUser] = useState<UserType | null>(initialData);
+
+    const contextValue: UserContextValueType = {
+        user,
+        setUser,
+    }
+
+    useEffect(() => {
+        if (user) {
+            localStorage.setItem('pod_user', JSON.stringify(user));
+        }else {
+            localStorage.removeItem('pod_user');
+        }
+    },[user])
+    return (
+        <UserContext.Provider value={contextValue}>
+            {children}
+        </UserContext.Provider>
+    )
+}
+ 
+export default UserProvider;
